6b4853ed7bf217d74bbbdabb9f8ec24584d1ef0b
J. Michael Brown <jmichaelbrown8@gmail.com>
2022-04-12 15:38:56 -0700

    Removed unused imports from Summary

--- client/src/Summary.js
+++ client/src/Summary.js
@@ -1,6 +1,4 @@
-import React, { useState } from "react";
 import { useSelector } from "react-redux";
-import store from "./store";
 
 export function Summary() {
   const rotations = useSelector((state) => state.rotations);
