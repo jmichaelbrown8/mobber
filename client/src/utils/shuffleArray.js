2298318f1550252f458f3c62eb3939213cb558aa
J. Michael Brown <jmichaelbrown8@gmail.com>
2022-04-12 19:18:14 -0700

    Implemented a shuffleArray function and a SET_MEMBERS reducer action

---
+++ client/src/utils/shuffleArray.js
@@ -0,0 +1,15 @@
+const shuffleArray = (array) => {
+  const multiplier = array.length;
+  if (multiplier <= 1) {
+    return array;
+  }
+  const randomIndex = Math.floor(Math.random() * multiplier);
+  return [
+    array[randomIndex],
+    ...shuffleArray(
+      array.slice(0, randomIndex).concat(array.slice(randomIndex + 1))
+    ),
+  ];
+};
+
+module.exports = shuffleArray;
